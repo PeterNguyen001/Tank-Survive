9b03b37c5da24cf2588ee57918e625e6e39b5e29
Peter_Nguyen <nguyenvietdung001@gmail.com>
2024-01-30 13:50:52 -0500

    Add Movement System and player movement

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Movement
+{
+    private Rigidbody2D playerRigidBody;
+    private float moveSpeed = 20f;
+    private float rotationSpeed = 180f;
+
+    public Movement(Rigidbody2D rb)
+    {
+        playerRigidBody = rb;
+    }
+
+    public void MoveTankForwardAndBackward(float verticalInput)
+    {
+        Vector2 moveForce = playerRigidBody.transform.up * verticalInput * moveSpeed;
+        playerRigidBody.AddForce(moveForce);
+    }
+
+    public void RotateTank(float horizontalInput)
+    {
+        float rotationAmount = -horizontalInput * rotationSpeed * Time.fixedDeltaTime;
+        playerRigidBody.AddTorque(rotationAmount);
+    }
+
+    public void SetMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+
+    public void SetRotationSpeed(float speed)
+    {
+        rotationSpeed = speed;
+    }
+
+    public void SetCanMove(bool move)
+    {
+        if (move)
+        {
+            playerRigidBody.velocity = Vector2.zero;
+            playerRigidBody.angularVelocity = 0f;
+        }
+    }
+}
